8db272854289b13b2f9de1b96867a86515ef286c
admin <admin@DESKTOP-QC2QO5O>
2022-02-23 11:57:00 +0530

    This Is My Oprator Programs.

---
+++ OPERATOR/src/Arthmati_operator.java
@@ -0,0 +1,13 @@
+
+public class Arthmati_operator {
+		public static void main(String args[])
+		{  
+		int a=100;  
+		int b=5;  
+		System.out.println(a+b);//15  
+		System.out.println(a-b);//5  
+		System.out.println(a*b);//50  
+		System.out.println(a/b);//2  
+		System.out.println(a%b);//0  
+		}
+		}  
---
+++ OPERATOR/src/Unary_operator.java
@@ -0,0 +1,8 @@
+public class Unary_operator{  
+public static void main(String args[]){  
+int x=108;  
+System.out.println(x++);//10 (11)  
+System.out.println(++x);//12  
+System.out.println(x--);//12 (11)  
+System.out.println(--x);//10  
+}}  

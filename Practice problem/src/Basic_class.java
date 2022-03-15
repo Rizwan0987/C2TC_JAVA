5f4662e4cc71593ac8e8df4f912d4159cd4932cd
admin <admin@DESKTOP-QC2QO5O>
2022-03-16 01:48:12 +0530

    This is my practice problem

---
+++ Practice problem/src/Basic_class.java
@@ -0,0 +1,10 @@
+class Basic_class {
+
+  // state or field
+  private int gear = 5;
+
+  // behavior or method
+  public void braking() {
+    System.out.println("Working of Braking");
+  }
+}
---
+++ Practice problem/src/Block_statement.java
@@ -0,0 +1,11 @@
+class Block_statement{
+    public static void main(String[] args) {
+    	
+        String band = "Beatles";
+    	
+        if (band == "Beatles") { // start of block
+            System.out.print("khan ");
+            System.out.print("rizwan");
+        } // end of block
+    }
+}
